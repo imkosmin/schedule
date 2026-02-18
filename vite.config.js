9ea3ce96829006235bcb1a27e73baf142e3d9bfd
imkosmin <72415818+imkosmin@users.noreply.github.com>
2026-02-18 17:31:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Replace 'your-repo-name' with the actual name of your GitHub repository
+  base: '/schedule/', 
 })
